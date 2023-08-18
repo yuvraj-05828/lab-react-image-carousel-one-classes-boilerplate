1af071ede5fceb07ad3a890e06ce64c6012926de
Kalvium <109285740+Kalvium-Program@users.noreply.github.com>
2023-08-18 14:08:39 +0530

    Add files via upload

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Carousel.css";
+import { images } from "../data/CarouselData";
+// you can explore more - and check as how to use materiul ui
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+// implement the class below
+class Carousel extends Component {
+  
+}
+
+export default Carousel;
